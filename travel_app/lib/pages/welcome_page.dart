1c1b1566cb9871c989aaff3bfb0d57751e81d837
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-01-31 15:32:14 +0530

    Input images into welcome page

---
+++ travel_app/lib/pages/welcome_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  List images = ["welcome_one.png", "welcome_two.png", "welcome_three.png"];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: images.length,
+          itemBuilder: (_, index) {
+            return Container(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("img/" + images[index]))),
+            );
+          }),
+    );
+  }
+}
